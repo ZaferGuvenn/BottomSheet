459b0a07424fe1c0e74a2c7f2e6676c0378a8b82
zaferguvenn <zafer.guven58@gmail.com>
2023-05-30 14:08:51 +0300

    Bottom sheet dialog with animation.

---
+++ app/src/main/java/com/lafimsize/bottomsheet/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.lafimsize.bottomsheet
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import com.lafimsize.bottomsheet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var dialog:Dialog
+
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bottomSheetDialogOpen.setOnClickListener {
+
+            openDialog()
+        }
+
+    }
+
+
+    private fun openDialog(){
+
+
+        dialog=Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        dialog.setContentView(R.layout.fragment_dialog)
+
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialog.window?.attributes?.windowAnimations=R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
+
+
+
+
+    }
+
+
+}
